91ce4b5d7ab2a5f6ec60926c54c5cf6f741a98dc
MegSherman <megoftheshermans@gmail.com>
2020-06-02 01:13:28 -0600

    through #5 with error

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 import Background from './../shared/Background/Background'
 import Chart1 from './../shared/Chart1';
 import Chart2 from './../shared/Chart2';
@@ -7,16 +8,22 @@ import DisplayPurchases from './../shared/DisplayPurchases';
 import Loading from './../shared/Loading/Loading';
 import Nav from './../shared/Nav';
 import './Budget.css';
+import { requestUserData } from './../../ducks/userReducer'
 
 
 class Budget extends Component {
+  componentDidMount () {
+    this.props.requestUserData()
+  }
 
   render() {
+    const {loading} = this.props.budget
+    const {firstName, lastName} = this.props.user
     return (
       <Background>
-        {true ? <Loading /> : null}
+        {loading ? <Loading /> : null}
         <div className='budget-container'>
-          <Nav />
+          <Nav firstName={firstName} lastName={lastName}/>
           <div className='content-container'>
             <div className="purchases-container">
               <AddPurchase />
@@ -33,4 +40,11 @@ class Budget extends Component {
   }
 }
 
-export default Budget;
+function mapStateToProps (state) {
+  return {
+    budget: state.budget,
+    user: state.user
+  }
+}
+
+export default connect (mapStateToProps, {requestUserData})(Budget);
